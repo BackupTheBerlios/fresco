293b6682f5572839ef075ce91067af46170ff90a
nicholasl <nicholasl>
2002-08-06 11:33:26 +0000

    Oops! Adding missing file.

---
+++ fresco-contrib/nicholasl/minesweeper/TerrainGenerator.hh
@@ -0,0 +1,11 @@
+#ifndef __TERRAINGENERATOR__
+#define __TERRAINGENERATOR__
+
+#include <sys/time.h>
+#include <time.h>
+#include <stdlib.h>
+#include <assert.h>
+
+int *cook_terrain(int width, int height, int mines);
+
+#endif
