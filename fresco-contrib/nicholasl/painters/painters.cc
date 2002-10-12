b697ed18c0fbddfe356ccf0234475d0dd38da3c9
nicholasl <nicholasl>
2002-10-12 16:18:51 +0000

    This might work when all triangles are on the far side of the origin. More testing needed.

--- fresco-contrib/nicholasl/painters/painters.cc
+++ fresco-contrib/nicholasl/painters/painters.cc
@@ -1,23 +1,33 @@
+// Fast Painters' Algorithm sort in -*- C++ -*-
+// Copyright (C) 2002 Nick Lewycky <nicholas@mxc.ca>
+// I'll LGPL it after the bugs are fixed.
+
 #include <Fresco/Types.hh>
-#include <strstream>
 #include <iostream>
 
 #include <algorithm>
 #include <iterator>
 #include <functional>
 
+#define DEBUG
+
 Fresco::Mesh cube();
 Fresco::Mesh test();
 void dumpMesh(Fresco::Mesh &mesh);
 
 // this is a "strict weak ordering" aka less than.
-class TriangleCompare : public binary_function<Fresco::Triangle&, Fresco::Triangle&, bool> {
+class TriangleCompare
+  : public std::binary_function<const Fresco::Triangle &, const Fresco::Triangle &, bool>
+{
 public:
   explicit TriangleCompare::TriangleCompare(Fresco::Vertices &ref_to_verseq)
     : vertices(ref_to_verseq) {}
   bool operator()(const Fresco::Triangle &one, const Fresco::Triangle &two) const
   {
     // returns true if one is on the wrong side of two, from Flights of Fantasy
+#ifdef DEBUG
+    std::cout << "performing test" << std::endl;
+#endif
 
     // test 1: do x extents overlap?
     Fresco::Coord x_one_min, x_one_max;
@@ -37,6 +47,9 @@ public:
     if (vertices[two.c].x > x_two_max) { x_two_max = vertices[two.c].x; }
 
     if ((x_two_min > x_one_max) || (x_two_max < x_one_min)) { return false; }
+#ifdef DEBUG
+    std::cout << "passed test 1" << std::endl;
+#endif
 
     // test 2: do y extents overlap?
     Fresco::Coord y_one_min, y_one_max;
@@ -56,22 +69,15 @@ public:
     if (vertices[two.c].y > y_two_max) { y_two_max = vertices[two.c].y; }
 
     if ((y_two_min > y_one_max) || (y_two_max < y_one_min)) { return false; }
+#ifdef DEBUG
+    std::cout << "passed test 2" << std::endl;
+#endif
 
     // The FoF tests 3 and 4 rely on planar coefficients for the triangles.
     // The catch is that a backfacing triangle will have a negated normal,
     // and will require a reverse comparison function. Here, we implement a
-    // backface detection algorithm and reverse the sign of the plane if
-    // necessary.
-
-    bool one_backfacing = 
-      (vertices[one.c].x*((vertices[one.a].z*vertices[one.b].y)-(vertices[one.a].y*vertices[one.b].z)) +
-       vertices[one.c].y*((vertices[one.a].x*vertices[one.b].z)-(vertices[one.a].z*vertices[one.b].x)) +
-       vertices[one.c].z*((vertices[one.a].y*vertices[one.b].x)-(vertices[one.a].x*vertices[one.b].y)))<0;
-
-    bool two_backfacing =
-      (vertices[two.c].x*((vertices[two.a].z*vertices[two.b].y)-(vertices[two.a].y*vertices[two.b].z)) +
-       vertices[two.c].y*((vertices[two.a].x*vertices[two.b].z)-(vertices[two.a].z*vertices[two.b].x)) +
-       vertices[two.c].z*((vertices[two.a].y*vertices[two.b].x)-(vertices[two.a].x*vertices[two.b].y)))<0;
+    // backface detection algorithm and reverse the sign of the coefficients
+    // if necessary.
 
     // test 3: is two entirely on the far (correct) side of one?
     Fresco::Coord two_A, two_B, two_C, two_D; // these are planar coefficients
@@ -92,8 +98,27 @@ public:
           + (-vertices[two.c].x)*(vertices[two.a].y*vertices[two.b].z -
 				  vertices[two.b].y*vertices[two.a].z);
 
-    if (two_backfacing)
+    if (two_D < 0)
       two_A*=-1; two_B*=-1; two_C*=-1; two_D*=-1;
+
+#ifdef DEBUG
+    if (two_A*vertices[two.a].x + two_B*vertices[two.a].y +
+	two_C*vertices[two.a].z + two_D != 0) {
+      std::cout << "bad math, point 2A" << std::endl;
+    }
+    if (two_A*vertices[two.b].x + two_B*vertices[two.b].y +
+	two_C*vertices[two.b].z + two_D != 0) {
+      std::cout << "bad math, point 2B" << std::endl;
+    }
+    if (two_A*vertices[two.c].x + two_B*vertices[two.c].y +
+	two_C*vertices[two.c].z + two_D != 0) {
+      std::cout << "bad math, point 2C" << std::endl;
+    }
+    std::cout << "plane two: " << two_A << "x + " << two_B << "y + "
+	      << two_C << "z + " << two_D << " = 0";
+    if (two_D < 0) std::cout << " and is backfacing";
+    std::cout << std::endl;
+#endif
 
     if (two_A*vertices[one.a].x + two_B*vertices[one.a].y +
 	two_C*vertices[one.a].z + two_D < 0) { return false; }
@@ -101,8 +126,12 @@ public:
 	two_C*vertices[one.b].z + two_D < 0) { return false; }
     if (two_A*vertices[one.c].x + two_B*vertices[one.c].y +
 	two_C*vertices[one.c].z + two_D < 0) { return false; }
+#ifdef DEBUG
+    std::cout << "passed test 3" << std::endl;
+#endif
 
     // test 4: is one entirely on the nearer (correct) side of two?
+
     Fresco::Coord one_A, one_B, one_C, one_D;
     one_A = (vertices[one.a].y)*(vertices[one.b].z - vertices[one.c].z)
           + (vertices[one.b].y)*(vertices[one.c].z - vertices[one.a].z)
@@ -120,8 +149,27 @@ public:
           + (-vertices[one.c].x)*(vertices[one.a].y*vertices[one.b].z -
                                   vertices[one.b].y*vertices[one.a].z);
 
-    if (one_backfacing)
+    if (one_D < 0)
       one_A*=-1; one_B*=-1; one_C*=-1; one_D*=-1;
+
+#ifdef DEBUG
+    if (one_A*vertices[one.a].x + one_B*vertices[one.a].y +
+	one_C*vertices[one.a].z + one_D != 0) {
+      std::cout << "bad math, point 1A" << std::endl;
+    }
+    if (one_A*vertices[one.b].x + one_B*vertices[one.b].y +
+	one_C*vertices[one.b].z + one_D != 0) {
+      std::cout << "bad math, point 1B" << std::endl;
+    }
+    if (one_A*vertices[one.c].x + one_B*vertices[one.c].y +
+	one_C*vertices[one.c].z + one_D != 0) {
+      std::cout << "bad math, point 1C" << std::endl;
+    }
+    std::cout << "plane one: " << one_A << "x + " << one_B << "y + "
+	      << one_C << "z + " << one_D << " = 0";
+    if (one_D < 0) std::cout << " and is backfacing";
+    std::cout << std::endl;
+#endif
 
     if (one_A*vertices[two.a].x + one_B*vertices[two.a].y +
 	one_C*vertices[two.a].z + one_D > 0) { return false; }
@@ -129,6 +177,9 @@ public:
 	one_C*vertices[two.b].z + one_D > 0) { return false; }
     if (one_A*vertices[two.c].x + one_B*vertices[two.c].y +
 	one_C*vertices[two.c].z + one_D > 0) { return false; }
+#ifdef DEBUG
+    std::cout << "passed test 4" << std::endl;
+#endif
 
     // skip test 5: do they actually occlude? Who cares, flip them anyways.
 
@@ -138,7 +189,9 @@ public:
     two_coef = 1./(two_A + two_B + two_C + two_D);
     if (one_A*one_coef == two_A*two_coef && one_B*one_coef == two_B*two_coef
 	&& one_C*one_coef == two_C*two_coef) {
-      //std::cout << "irreflexivity case caught!" << std::endl;
+#ifdef DEBUG
+      std::cout << "irreflexivity case caught!" << std::endl;
+#endif
       return false;
     }
 
@@ -148,20 +201,19 @@ private:
   Fresco::Vertices vertices;
 };
 
-class TriangleIterator : public random_access_iterator<Fresco::Triangle, ptrdiff_t>{
+class TriangleIterator
+  : public std::random_access_iterator<Fresco::Triangle, ptrdiff_t>
+//  : public std::iterator<std::random_access_iterator_tag, Fresco::Triangle>
+{
 public:
-  TriangleIterator::TriangleIterator(Fresco::TriangleSeq &ref_to_seq, const ptrdiff_t i)
+  TriangleIterator::TriangleIterator(Fresco::TriangleSeq &ref_to_seq, ptrdiff_t i)
     : seq(&ref_to_seq), index(i) {}
   TriangleIterator operator++(int) { // post-increment
-    TriangleIterator old(*this);
-    ++index;
-    return old;
+    return TriangleIterator(*seq, index++);
   }
   TriangleIterator &operator++() { ++index; return *this; }
   TriangleIterator operator--(int) { // post-decrement
-    TriangleIterator old(*this);
-    --index;
-    return old;
+    return TriangleIterator(*seq, index--);
   }
   TriangleIterator &operator--() { --index; return *this; }
   Fresco::Triangle &operator[] (ptrdiff_t i) const {
@@ -175,16 +227,17 @@ public:
   bool operator==(const TriangleIterator&that) const {
     return (this->index == that.index);
   }
+  bool operator!=(const TriangleIterator&that) const {
+    return (this->index != that.index);
+  }
   TriangleIterator operator+(const ptrdiff_t n) const {
-    TriangleIterator sum(*seq, index+n);
-    return sum;
+    return TriangleIterator(*seq, index+n);
   }
   ptrdiff_t operator-(const TriangleIterator& that) const {
     return this->index - that.index;
   }
   TriangleIterator operator-(const ptrdiff_t n) const {
-    TriangleIterator difference(*seq, index-n);
-    return difference;
+    return TriangleIterator(*seq, index-n);
   }
 private:
   ptrdiff_t index;
@@ -201,13 +254,21 @@ bool cirr()
   mesh.nodes[1].x = 1; mesh.nodes[1].y = 0; mesh.nodes[1].z = 0;
   mesh.nodes[2].x = 1; mesh.nodes[2].y = 1; mesh.nodes[2].z = 0;
 
-  mesh.triangles.length(2);
+  mesh.triangles.length(4);
   mesh.triangles[0].a = 0; mesh.triangles[0].b = 1; mesh.triangles[0].c = 2;
   mesh.triangles[1].a = 0; mesh.triangles[1].b = 2; mesh.triangles[1].c = 1;
+  mesh.triangles[2].a = 2; mesh.triangles[2].b = 0; mesh.triangles[2].c = 1;
+  mesh.triangles[3].a = 1; mesh.triangles[3].b = 2; mesh.triangles[3].c = 0;
 
   TriangleCompare comp(mesh.nodes);
 
-  return (comp(mesh.triangles[0], mesh.triangles[1]) == false);
+  for(int i = 0; i < 4; i++) {
+    for(int j = 0; j < 4; j++) {
+      if (comp(mesh.triangles[i], mesh.triangles[j]) == false)
+	return true;
+    }
+  }
+  return false;
 }
 
 int main(int argc, char *argv[])
@@ -216,9 +277,11 @@ int main(int argc, char *argv[])
   CORBA::ORB_var orb;
   orb = CORBA::ORB_init(argc, argv);
 
+#if 0
   if (!cirr()) {
     std::cout << "complex irreflexivity test failed." << std::endl;
   }
+#endif
 
   //Fresco::Mesh mesh = cube();
   Fresco::Mesh mesh = test();
@@ -229,7 +292,7 @@ int main(int argc, char *argv[])
   TriangleIterator last(mesh.triangles, mesh.triangles.length());
   Fresco::Vertices v = mesh.nodes; // transform the vertices here
   TriangleCompare comp(v);
-  sort(first, last, comp);
+  std::sort(first, last, comp);
   std::cout << std::endl << "after:" << std::endl;
   dumpMesh(mesh);
 
@@ -252,7 +315,7 @@ Fresco::Mesh test()
   mesh.nodes[5].x = 1; mesh.nodes[5].y = 1; mesh.nodes[5].z = 1;
 
   mesh.triangles.length(2);
-  mesh.triangles[0].a = 3; mesh.triangles[0].b = 5; mesh.triangles[0].c = 4;
+  mesh.triangles[0].a = 3; mesh.triangles[0].b = 4; mesh.triangles[0].c = 5;
   mesh.triangles[1].a = 0; mesh.triangles[1].b = 1; mesh.triangles[1].c = 2;
 
   return mesh;
@@ -311,7 +374,9 @@ Fresco::Mesh cube()
   return mesh;
 }
 
-class dumpTriangle : public unary_function<Fresco::Triangle&, void> {
+class dumpTriangle
+  : public std::unary_function<const Fresco::Triangle&, void>
+{
 public:
   void operator()(const Fresco::Triangle &t) const {
     std::cout << "(" << t.a << "," << t.b << "," << t.c << ")" << std::endl;
@@ -323,6 +388,9 @@ void dumpMesh(Fresco::Mesh &mesh)
   TriangleIterator i(mesh.triangles, 0);
   TriangleIterator end(mesh.triangles, mesh.triangles.length()); 
   dumpTriangle dt;
-  for_each(i, end, dt);
+  std::for_each(i, end, dt);
 }
 
+// Local Variables:
+// mode:C++
+// End:
--- fresco-contrib/nicholasl/painters/painters.sh
+++ fresco-contrib/nicholasl/painters/painters.sh
@@ -1,3 +1,5 @@
 #! /bin/sh
-source ../fresco-config
-$CPP $CPPFLAGS $CXXFLAGS painters.cc -o painters $LIBFLAGS
+#source ../fresco-config
+#CPP="g++-3.2"
+#$CPP $CPPFLAGS $CXXFLAGS painters.cc -o painters $LIBFLAGS
+c++ -D_REENTRANT -I/home/nicholas/Fresco/Prague/include -I/home/nicholas/Fresco/Prague/include -I/home/nicholas/Fresco/Babylon/include -I/home/nicholas/Fresco/Babylon/include -I/home/nicholas/Fresco/include painters.cc -o painters -L/home/nicholas/Fresco/lib -lFresco -lomnithread -ltcpwrapGK -lomniORB3 -lomniDynamic3 -lpthread -L/home/nicholas/Fresco/Babylon/lib -lBabylon -L/home/nicholas/Fresco/Prague/lib -lPrague -ldl -lpthread -lutil
